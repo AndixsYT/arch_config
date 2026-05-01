5ebbe81cd9ba21e6de2c5ec1663ba5552de72b1b
andixs <lukassterner006@gmail.com>
2026-05-01 15:22:32 +0200

    full hypr config

---
+++ .config/hypr/configs/autostart.lua
@@ -0,0 +1,9 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+hl.on("hyprland.start", function()
+	hl.exec_cmd("awww-daemon --format xrgb")
+	hl.exec_cmd("swaync")
+	hl.exec_cmd("skwd-daemon")
+	hl.exec_cmd("bash ~/Scripts/cwal.sh")
+end)
---
+++ .config/hypr/configs/general.lua
@@ -0,0 +1,64 @@
+----------------
+---- CURSOR ----
+----------------
+
+hl.config({
+	cursor = {
+		no_hardware_cursors = 0,
+		inactive_timeout = 1,
+		hide_on_key_press = 1,
+	},
+})
+
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+	input = {
+		kb_layout = "us",
+
+		repeat_rate = 50,
+		repeat_delay = 300,
+
+		follow_mouse = 1,
+	},
+})
+
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+hl.env("XCURSOR_SIZE", "40")
+hl.env("HYPRCURSOR_SIZE", "40")
+
+hl.config({
+	dwindle = {
+		preserve_split = true, -- You probably want this
+	},
+})
+
+hl.config({
+	master = {
+		new_status = "master",
+	},
+})
+
+hl.config({
+	scrolling = {
+		fullscreen_on_one_column = true,
+	},
+})
+
+----------------
+----  MISC  ----
+----------------
+
+hl.config({
+	misc = {
+		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
+		disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
+		disable_splash_rendering = true,
+		middle_click_paste = false,
+	},
+})
